0da26d06bd278b870f3878e007553b5ce146bb44
varsha s <varshas@varshas-MacBook-Pro.local>
2025-01-17 21:12:01 +0530

    cool

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -5,99 +5,10 @@ import signUp from "../../assets/Icons/sign-up.svg";
 import whiteArrow from "../../assets/Icons/white-arrow.svg";
 import creditCardOff from "../../assets/Icons/credit-card-off.svg";
 import BrandScroll from "@components/BrandScroll";
-import retailStore1 from "../../assets/Icons/retail-store-1.svg";
-import retailStore2 from "../../assets/Icons/retail-store-2.svg";
-import retailStore3 from "../../assets/Icons/retail-store-3.svg";
-import verifiedPhoneNumber from "../../assets/Icons/verified-phonenumber.svg";
-import onboarding from "../../assets/Icons/onboarding.svg";
-import support from "../../assets/Icons/full-support.svg";
-import creator from "../../assets/Icons/creator-collaboration.svg";
-import testimonial1 from "../../assets/Icons/landing-testimonial-1.svg";
-import testimonial2 from "../../assets/Icons/landing-testimonial-2.svg";
-import testimonial3 from "../../assets/Icons/landing-testimonial-3.svg";
-import Aachi from "../../assets/Icons/aachiColor.svg";
-import vgn from "../../assets/Icons/vgnColor.svg";
-import Diedam from "../../assets/Icons/DiaDem-testi.svg";
+
 import { useKeenSlider } from "keen-slider/react";
 import marketingStrategy from "../../assets/Icons/marketingStrategy.svg";
-
-export const retailData = [
-  {
-    id: 1,
-    img: retailStore1,
-    title: "Use WhatsApp to Converse to Convert",
-    subTitle:
-      "Reach your target audience directly on their preferred platform.",
-  },
-  {
-    id: 2,
-    img: retailStore2,
-    title: "Personalized Customer Journeys",
-    subTitle:
-      "Create tailored experiences for each customer, building lasting relationships.",
-  },
-  {
-    id: 3,
-    img: retailStore3,
-    title: "Drive Continuous Engagement",
-    subTitle:
-      "Streamline conversations, schedule appointments, and nurture leads effortlessly.",
-  },
-];
-
-export const whyGainwix = [
-  {
-    id: 1,
-    img: verifiedPhoneNumber,
-    title: "Verified WhatsApp Number",
-    text: "Build trust with a verified business account.",
-  },
-  {
-    id: 2,
-    img: onboarding,
-    title: "Personalized Onboarding",
-    text: "Engage customers with tailored messages.",
-  },
-  {
-    id: 3,
-    img: support,
-    title: "24x7x365 Support",
-    text: "Handle inquiries 24/7, even when you're offline.",
-  },
-  {
-    id: 4,
-    img: creator,
-    title: "Creator Collabration",
-    text: "Guide customers through sales funnels and answer questions effortlessly.",
-  },
-];
-
-export const testimonialData = [
-  {
-    id: 1,
-    img: testimonial2,
-    logo: Aachi,
-    text: "The AI-powered marketing solutions from Gainwix.ai especially through WhatsApp, have revolutionized the way we interact with our customers. Gainwix Consulting's insights have enabled us to scale our campaigns effectively, resulting in impressive growth and engagement.",
-    name: "Harish",
-    position: "Managing Director",
-  },
-  {
-    id: 2,
-    img: testimonial3,
-    logo: Diedam,
-    text: "Using Gainwix.ai has transformed the way we run our retail and ecommerce business. As a fashion apparel seller, connecting with customers quickly and efficiently is crucial. With Gainwix.ai ‘s AI-powered WhatsApp automation, we’ve boosted our sales significantly by engaging our customers in real time, sending personalized messages, and streamlining order processes. It’s like having a 24/7 sales assistant that never misses a beat. Highly recommend it for any business looking to scale effortlessly!",
-    name: "Pown",
-    position: "Digital Marketing Manager",
-  },
-  {
-    id: 3,
-    img: testimonial1,
-    logo: vgn,
-    text: "Working with Gainwix.ai has transformed our marketing approach. Their AI solutions and expert guidance through Gainwix Consulting have streamlined our communication across platforms, allowing us to reach our target audience with precision. Our campaigns have never been more efficient, and our ROAS has soared.",
-    name: "Mathesh",
-    position: "Sales Manager",
-  },
-];
+import { retailData, whyGainwix, testimonialData } from "../blogs/constants/blog"
 
 const Blogs = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
